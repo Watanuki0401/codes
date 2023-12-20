8847700247a0f76311b2c072839b0bf50b1b7624
Ashihara <1cda1201@mail.u-tokai.ac.jp>
2023-12-20 15:23:44 +0000

    log.18

--- nextjs/tutorial/routing-demo/src/app/products/[productId]/page.tsx
+++ nextjs/tutorial/routing-demo/src/app/products/[productId]/page.tsx
@@ -1,7 +1,25 @@
-export default function ProductsDetails( { 
-    params, 
- }: {
-    params: {productId: string};
- }) {
+import { Metadata } from "next";
+import { resolve } from "path";
+
+type Props = {
+   params: {
+      productId: string;
+   };
+};
+
+export const generateMetadata = async ({
+   params,
+}: Props): Promise<Metadata> => {
+   const title = await new Promise(resolve => {
+      setTimeout(() => {
+         resolve(`iPhone ${params.productId}`)
+      }, 100)
+   })
+   return {
+      title: `Product ${title}`,
+   }
+}
+
+export default function ProductsDetails( { params }: Props) {
     return <h1>Details about product {params.productId} </h1>;
 }
